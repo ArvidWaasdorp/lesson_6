f3b1cfb509d6e699dfb4751ba77573a3afebf8a2
Arvid Waasdorp <a.waasdorp@gmail.com>
2016-10-05 20:32:25 +0200

    changed design and clean-up CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,97 @@
 
 $( document ).ready(function() {
  
-  var random;     //A random number to initiate a animation
-
   //Just show the button
-  $("#clickMe").fadeIn (3000);
+  $('#clickMe').fadeIn (3000);
 
+  //Action what happends when the button is loaded
+  $('button').click(function(){
 
+    var random; //A random number to initiate a animation
+    random = Math.floor(Math.random() * 4);
 
-  $("button").click(function(){
+    switch (random) {
+      case 0:
+        scrollNick();
+        break;
+      case 1:
+        fadeJenny();
+        break;
+      case 2:
+        increaseFont();
+        break;
+      case 3:
+        slideDiv();
+        break;
+      default:
+        break;   
 
-//   fadeJenny();
-      
-  $('#imgN').finish().css('width', '319px').css('left', '0');
-    $("#imgN").fadeIn("300", 0.00, function(){
+    }
 
-      $(this).animate({
-        marginLeft : "100%", //moves right
-        width: '0'
-      });
-
-
+/*
+    $('#box1').click(function(){
+      background-color: #e5eecc
     });
+*/
+
+
+    //Debug
+    console.log ('Random number: ' + random);
 
   });
 });
 
 
 function fadeJenny () {
+
+  //reset the CSS styles
   $('#imgJ').finish().css('width', '319px').css('height', '167px');
-  $("#imgJ").fadeIn("300", 0.00, function(){ //fade
 
-      $(this).animate({
-          opacity: '0.0',
-          height: '90%',
-          width: '90%'
-      });
+  //Fade in
+  $('#imgJ').fadeIn('300', 0.00, function(){    
 
-    $(this).fadeToggle("slow");
-    $(this).stop();
+    //increase the page
+    $(this).animate({
+        opacity: '0.0',
+        height: '90%',
+        width: '90%'
+    });
+    
+    $(this).fadeOut('slow');      //Fade out
+    $(this).stop();               //Stop the animation
+
   });
+}
+
+function scrollNick () {
+  var newTop;   //
+  newTop = Math.floor(Math.random() * 60) + '%';
+
+  //reset the CSS styles and set the new height of the image
+  $('#imgN').finish().css('width', '478px').css('marginLeft', '0px').css('top', newTop).css('float', 'left').css('left', '0px');
+
+  //Fade in
+  $('#imgN').fadeIn('300', 0.00, function(){
+
+    //moves right
+    $(this).animate({
+      marginLeft : '100%', 
+      width: '0'
+    }, 'slow');
+    
+    $(this).stop();               //Stop the animation
+  });
+}
+
+function increaseFont () {
+  if ($('#pFont').css('fontSize') === '14px')
+  {
+    $('#pFont').animate({fontSize: '20px'}, 'slow');
+  } else {
+    $('#pFont').animate({fontSize: '14px'}, 'slow');
+  }
+}
+
+function slideDiv () {
+  $("#slideToggle").slideToggle("slow");
 }
