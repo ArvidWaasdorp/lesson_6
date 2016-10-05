89bf87c4ed4955029cf4fe2e8d08aa587edb9073
Arvid Waasdorp <a.waasdorp@gmail.com>
2016-10-05 23:16:41 +0200

    Finished assignment, 2 files with and without formatting

--- scripts/script.js
+++ scripts/script.js
@@ -3,41 +3,50 @@ $( document ).ready(function() {
  
   //Just show the button
   $('#clickMe').fadeIn (3000);
-  $('#boxes').slideDown (30);
+  $('#boxes').slideDown (3000);
 
 
   //Action what happends when the button is loaded
   $('button').click(function(){
 
     var random; //A random number to initiate a animation
-    random = Math.floor(Math.random() * 5);
+    random = Math.floor(Math.random() * 7);
 
-    switch (random) {
-      case 0:
-        scrollNick();
-        break;
-      case 1:
-        fadeJenny();
-        break;
-      case 2:
-        increaseFont();
-        break;
-      case 3:
-        $('#pFade').fadeToggle('slow');
-        break;
-      case 4:
-        $('#iBird').slideToggle('slow');;
-        break;
-      case 5:
-        break;
-      default:
-        break;   
+
+    if (!$('#boxes').is(':visible')) {
+      alert ("It is not possible to use this page when the boxes are hidden. Let me show them to continue....");
+        
+      $('#boxes').show (1500);
+    } else {
+
+      switch (random) {
+        case 0:
+          scrollNick();
+          break;
+        case 1:
+          fadeJenny();
+          break;
+        case 2:
+          increaseFont();
+          break;
+        case 3:
+          fadeText();
+          break;
+        case 4:
+          slideImage();
+          break;
+        case 5:
+          changeColor();
+          break;
+        case 6:
+          hideBoxes();
+          break;
+        default:
+          break;   
+
+      }
 
     }
-
-    //Debug
-    console.log ('Random number: ' + random);
-
   });
 
   //case 2
@@ -47,29 +56,56 @@ $( document ).ready(function() {
 
   //case 3
   $('#dFade').click(function(){
-    $('#pFade').fadeToggle('slow');
+    fadeText();
   });
 
   // case 5
   $('#dBird').click(function(){
-    $('#iBird').slideToggle('slow');
+    slideImage();
   });
 
   //case 6
-  $('#box2').click(function(){
-    var colors;
-    colors = ['#C5D5EA', '#759EB8', '#7392B7', '#B3C5D7', '#D8E1E9'];
-
-    $(this).delay(3000).css('background-color', '#7392B7');
-
-      $(this).css("background-color", "red");
- 
-
+  $('#dChangeColor').click(function(){
+    changeColor();
   });
 
 
+  //Change the text of the box when mouseOver and change it back when mouseOut
+  $('#dChangeText').mouseover(function() {
+   
+    $('#pChangeText').html ('<a href=\"http://www.urbandictionary.com/define.php?term=resistance%20is%20futile\" target=\"_blank\">Resistance is futile</a>. <br>(Link is not working because the script thinks I leave the box :-( )');
 
+  });
+ 
+  $('#dChangeText').mouseout(function() {
 
+     $('#pChangeText').html ('Hello world!');
+
+  });
+  //Change the text of the box when mouseOver and change it back when mouseOut
+
+  //case 7
+  $('#dCleanUp').click(function(){
+    hideBoxes();
+  });
+
+  //Box 7-9
+  $('#dAddText').click(function(){
+
+    var txtPrep   = $('<span class="prepended"></span>').text('Prepended text');     // Create with jQuery
+    var txtAppend = $('<span class="append"></span>').text('Append text');           // Create with jQuery
+
+     $('#dText').prepend(txtPrep);
+     $('#dText').append(txtAppend);
+
+  });
+
+  $('#dDelText').click(function(){
+    $(".prepended").remove();
+    $(".append").remove();
+  });
+  //Box 7-9
+  
 });
 
 
@@ -124,5 +160,29 @@ function increaseFont () {
 }
 
 function slideDiv () {
-  $("#slideToggle").slideToggle("slow");
+  $('#slideToggle').slideToggle('slow');
+}
+
+function changeColor() {
+  var colors;   //Array of colors
+  var random;   //A random number to initiate a animation
+
+
+  colors = ['#373854', '#493267', '#9e379f', '#e86af0', '#7bb3ff'];
+  random = Math.floor(Math.random() * colors.length) - 1;
+
+  $('#dChangeColor').css ('background-color', colors[random]);
+  $('#dChangeColor').stop(); 
+}
+
+function fadeText() {
+  $('#pFade').fadeToggle('slow');
+}
+
+function slideImage() {
+  $('#iBird').slideToggle('slow');
+}
+
+function hideBoxes() {
+  $('#boxes').hide (1500);
 }
